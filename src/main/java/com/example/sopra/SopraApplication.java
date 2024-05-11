d3916895f4c6ebf01c16a885c9af715d038fa52a
Jonas Lutz <jplutz@t-online.de>
2024-05-11 17:06:29 +0200

    Intial commit

---
+++ src/main/java/com/example/sopra/SopraApplication.java
@@ -0,0 +1,13 @@
+package com.example.sopra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SopraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SopraApplication.class, args);
+    }
+
+}
