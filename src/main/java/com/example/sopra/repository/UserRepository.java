9704ecd7409a076de22c2b3b75d000eb80ad9251
nijade <166016821+nijade@users.noreply.github.com>
2024-05-15 12:33:56 +0200

    Added unimplemented Method findByUsername to UserRepository

--- src/main/java/com/example/sopra/repository/UserRepository.java
+++ src/main/java/com/example/sopra/repository/UserRepository.java
@@ -3,6 +3,7 @@ package com.example.sopra.repository;
 import com.example.sopra.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
  public interface UserRepository extends JpaRepository<User, Integer> {
+  User findByUsername(String username);
 
  }
 
