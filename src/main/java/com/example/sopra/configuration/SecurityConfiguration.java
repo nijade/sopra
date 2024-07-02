81f194f783bac3c7fb4f38f44731de2b9e6c3912
lino-antonino <lino.mu@outlook.de>
2024-07-02 15:05:18 +0200

    i18n update in security
    rebuilt i18n because of of merge conflict

--- src/main/java/com/example/sopra/configuration/SecurityConfiguration.java
+++ src/main/java/com/example/sopra/configuration/SecurityConfiguration.java
@@ -10,11 +10,18 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @Configuration
 @EnableWebSecurity
 @EnableScheduling
-public class SecurityConfiguration {
+public class SecurityConfiguration implements WebMvcConfigurer {
 
 
     @Bean
@@ -69,5 +76,24 @@ public class SecurityConfiguration {
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        var resolver = new SessionLocaleResolver();
+        resolver.setDefaultLocale(Locale.GERMAN);
+        return resolver;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        var lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
     }
 }
